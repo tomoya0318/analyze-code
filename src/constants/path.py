d6be01e1d13b69570084a7151855c5858fe95007
tomoya-n <s266227@wakayama-u.ac.jp>
2024-01-22 11:04:21 +0900

    pathの参照方法の変更

--- src/constants/path.py
+++ src/constants/path.py
@@ -1,6 +1,9 @@
+"""使用するPATHの一覧
+"""
 import os
 
-ROOT = os.path.abspath("../../")
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 DATA = f'{ROOT}/data'
 SRC = f'{ROOT}/src'
 OUT = f'{DATA}/out'
