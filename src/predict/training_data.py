be1cd732717faa092f3153cfd43184400972b1b1
tomoya-n <s266227@wakayama-u.ac.jp>
2024-01-29 17:59:57 +0900

    規約IDと修正可非のcsv出力の完成

--- src/predict/training_data.py
+++ src/predict/training_data.py
@@ -1,18 +1,45 @@
 import pandas as pd
+import os
 import warnings
 from sklearn.model_selection import train_test_split
 from constants import path
 warnings.filterwarnings("always", category=UserWarning)
 
-def extract_training_data(explanatory_variable, project_name): 
-    """説明変数の学習用データの抽出
+def main():
+    #プロジェクトの選択
+    project_list = []
+    with open(f'{path.DATA}/white_list.txt', 'r') as f:
+        for line in f:
+            project_list.append(line.strip())
+
+    #すべてのプロジェクトで，学習データの取得
+    for project_name in project_list:
+        try:
+            df_value = pd.read_csv(f"{path.ML}/{project_name}_value.csv")
+            df_label = pd.read_csv(f"{path.ML}/{project_name}_label.csv", header=None)
+            extract_training_data(df_value, df_label, project_name)
+        except pd.errors.EmptyDataError as e:
+            print(project_name)
+    
+def extract_training_data(explanatory_variable, label, project_name): 
+    """目的変数の学習用データの抽出
 
     Args:
         explanatory_variable (dataframe): 説明変数
         project_name (str): プロジェクト名
         model_name (str): モデルの名前
     """
+
+    #ディレクトリの確認
+    dir = f'{path.PRERESULT}/raw_model'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
     #学習用データの抽出
-    df_marge = pd.DataFrame(explanatory_variable)
-    X_train, _ = train_test_split(df_marge, test_size=0.2, shuffle=False)
-    X_train.to_csv(f"{path.PRERESULT}/row_model/{project_name}_train.csv")
+    X_train, _, Y_train, _ = train_test_split(explanatory_variable, label, test_size=0.2, shuffle=False)
+    Y_train.columns = ['Target'] 
+    train_df = pd.concat([X_train['Warning ID'], Y_train], axis=1)
+    train_df.to_csv(f"/{dir}/{project_name}_train.csv", index=False)
+
+if __name__ == '__main__':
+    main()
