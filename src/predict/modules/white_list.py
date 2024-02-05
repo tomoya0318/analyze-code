39a69a96597b24d1634a7faecfd7f28efe227b8a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-05 16:14:29 +0900

    white_listの追加

---
+++ src/predict/modules/white_list.py
@@ -0,0 +1,17 @@
+def lookup_white_list(txt_file):
+    """ホワイトリストに設定されたプロジェクトの名称取得
+
+    Args:
+        txt_file (str): ホワイトリストを書いているファイルへのpath
+
+    Returns:
+        list: プロジェクトのリスト
+    """
+
+    #プロジェクトの選択
+    project_list = []
+    with open(txt_file, 'r') as f:
+        for line in f:
+            project_list.append(line.strip())
+
+    return project_list
