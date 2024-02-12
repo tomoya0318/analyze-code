70cec8b59c87597f0ac11d53a7dadffa71d0c240
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-12 19:20:53 +0900

    コサイン類似度を計算するプログラムの追加

--- src/cosin/correct_from_model.py
+++ src/cosin/correct_from_model.py
@@ -1,8 +1,27 @@
 import pandas as pd
+import numpy as np
 from utils import lookup_white_list
-from utils import cos_sim
 from constants import path
 
+def __insert_key_if_none(v1, v2):
+    """比較対象先に特定のキーが含まれていない場合，値を0として追加し，更新された比較先の辞書の値だけをリストとして返す．
+
+    Args:
+        v1 (dict): 比較先の辞書
+        v2 (dict): 比較元の辞書
+
+    Returns:
+        list: 比較先の辞書に含まれる値のリスト
+    """
+    for key in v2:
+        if key not in v1:
+            v1[key] = 0
+    
+    sorted_v1_keys = sorted(v1.keys())
+    sorted_v1_values = [v1[key] for key in sorted_v1_keys]
+
+    return sorted_v1_values
+        
 def lookup_from_model(csvfile):
     convention_dict = {}
     total_convention = 0
@@ -25,3 +44,10 @@ def lookup_from_model(csvfile):
         convention_dict[key] = round(value[0] / value[1], 2)
 
     return dict(sorted(convention_dict.items()))
+
+def calc_cosin(v1, v2):
+    v1_out = __insert_key_if_none(v1, v2)
+    v2_out = __insert_key_if_none(v2, v1)
+
+    return np.dot(v1_out, v2_out) / (np.linalg.norm(v1_out) * np.linalg.norm(v2_out))
+    
