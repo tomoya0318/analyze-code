34331aeb53eaadbed885b8f0ceda5e2905e8e648
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-17 14:19:51 +0900

    create main

--- src/cosin/correct_from_model.py
+++ src/cosin/correct_from_model.py
@@ -1,7 +1,36 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 from utils import lookup_white_list
 from constants import path
+def main():
+    project_list = lookup_white_list(path.WHITELIST)
+    df = pd.DataFrame()
+
+    for i, name1 in tqdm(enumerate(project_list), total=len(project_list)):
+        for name2 in tqdm((project_list[i + 1:]), desc=f"comparing with {name1}", leave=False):
+            name1_dict = lookup_from_model(f"{path.PRERESULT}/raw_model/{name1}_train.csv")
+            name2_dict = lookup_from_model(f"{path.PRERESULT}/raw_model/{name2}_train.csv")
+
+            cosin_sim, nan_count = calc_cosin(name1_dict, name2_dict)
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame(
+                        {
+                            "Project 1": [name1],
+                            "fix_count1": [sum(item[0] for item in name1_dict.values())],
+                            "Project 2": [name2],
+                            "fix_count2": [sum(item[0] for item in name2_dict.values())],
+                            "cos_sim": [cosin_sim],
+                            "not_nan": [nan_count],
+                        }
+                    ),
+                ],
+                ignore_index=True,
+            )
+    
+    df.to_csv(f"{path.OUT}/all_cosin_similarity.csv", encoding='utf-8', index=False)
 
 def __insert_key_if_none(v1, v2):
     """比較対象先に特定のキーが含まれていない場合，値を0として追加し，更新された比較先の辞書の値だけをリストとして返す．
@@ -15,12 +44,9 @@ def __insert_key_if_none(v1, v2):
     """
     for key in v2:
         if key not in v1:
-            v1[key] = 0
+            v1[key] = None
     
-    sorted_v1_keys = sorted(v1.keys())
-    sorted_v1_values = [v1[key] for key in sorted_v1_keys]
-
-    return sorted_v1_values
+    return [v1[key] for key in sorted(v1)]
         
 def lookup_from_model(csvfile):
     """csvファイルから規約の修正率を取得
@@ -31,26 +57,14 @@ def lookup_from_model(csvfile):
     Returns:
         dict: {Warning ID, [fixed_convention, total_convention]}という辞書型で返す
     """
-    convention_dict = {}
-    total_convention = 0
-    fixed_convention = 0
     df = pd.read_csv(csvfile)
-    warning_id = df["Warning ID"].tolist()
-    convention_exist = df["Target"].tolist()
+    convention_dict = {}
 
-    for i in range(len(warning_id)):
-        if convention_exist[i] == 1:
-            fixed_convention += 1
-
-        total_convention += 1
-        convention_dict[warning_id[i]] = [fixed_convention, total_convention]
-
-    # coding_convention_distの値をパーセントに変更(有効数字2桁)
-    for key, value in convention_dict.items():
-        if value[1] == 0:
-            continue
-        convention_dict[key] = round(value[0] / value[1], 2)
-
+    for warning_id in df["Warning ID"].unique():
+        warning_data = df[df["Warning ID"] == warning_id]
+        fixed_convention = warning_data["Target"].sum()
+        total_convention = warning_data["Target"].count()
+        convention_dict[warning_id] = [fixed_convention, total_convention]
     return dict(sorted(convention_dict.items()))
 
 def calc_cosin(v1, v2):
@@ -63,8 +77,20 @@ def calc_cosin(v1, v2):
     Returns:
         float: 2つのプロジェクトの規約修正率のコサイン類似度
     """
-    v1_out = __insert_key_if_none(v1, v2)
-    v2_out = __insert_key_if_none(v2, v1)
 
-    return np.dot(v1_out, v2_out) / (np.linalg.norm(v1_out) * np.linalg.norm(v2_out))
+    v1 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v1.items()}
+    v2 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v2.items()}
+
+    v1_values = __insert_key_if_none(v1, v2)
+    v2_values = __insert_key_if_none(v2, v1)
     
+    v1_nan_0 = [0 if pd.isna(value) else value for value in v1_values]
+    v2_nan_0 = [0 if pd.isna(value) else value for value in v2_values]
+
+    cosin_sim = format(np.dot(v1_nan_0, v2_nan_0) / (np.linalg.norm(v1_nan_0) * np.linalg.norm(v2_nan_0)), ".2f")
+    nan_count = sum(1 for value1, value2 in zip(v1_values, v2_values) if not pd.isna(value1) and not pd.isna(value2))
+
+    return cosin_sim, nan_count 
+    
+if __name__ == "__main__":
+        main()
