050a68fdaede2a0443f4e87c4281ca13fdd0728c
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-18 11:16:15 +0900

    few change

--- src/predict/main.py
+++ src/predict/main.py
@@ -3,14 +3,6 @@ import pandas as pd
 from predict import ModelCreater
 from constants import path
 from utils import lookup_white_list
-# def single_all():
-#     model_name = "RandomForest"
-#     project_list = lookup_white_list(f"{path.DATA}/white_list.txt")
-#     for project_name in tqdm(project_list):
-#         mc = ModelCreater(model_name)
-#         model, dummys = mc.fit_single_model(project_name)
-#         result_all, _ = mc.predict_single_model(project_name, model, dummys)
-#         result_all.to_csv(f"{path.PRERESULT}/single/{model_name}/all/{project_name}.csv")
 
 def single_convention():
     model_name = "RandomForest"
@@ -21,22 +13,6 @@ def single_convention():
         _, result_convention = mc.predict_single_model(project_name, model, dummys)
         result_convention.to_csv(f"{path.PRERESULT}/single/{model_name}/violations/{project_name}.csv")
 
-def merge_all():
-    model_name = "RandomForest"
-    project_list = lookup_white_list(path.WHITELIST)
-    for i in tqdm(range((len(project_list)))):
-        for j in range(i + 1, len(project_list)):
-            merge_list = [project_list[i],project_list[j]]
-            mc = ModelCreater(model_name)
-            model, dummys = mc.fit_merge_model(merge_list)
-            result_all = mc.predict_merge_model(merge_list, model, dummys)
-
-            merge1 = pd.DataFrame([result_all[merge_list[0]][0]], index = [merge_list[0]])
-            merge1.to_csv(f"{path.PRERESULT}/merge/{model_name}/all/{merge_list[0]}_merge_{merge_list[0]}_{merge_list[1]}.csv")
-
-            merge2 = pd.DataFrame([result_all[merge_list[1]][0]], index = [merge_list[1]])
-            merge2.to_csv(f"{path.PRERESULT}/merge/{model_name}/all/{merge_list[1]}_merge_{merge_list[0]}_{merge_list[1]}.csv")
- 
 def all():
     model_name = "RandomForest"
     project_list = lookup_white_list(path.WHITELIST)
