268f62938dd60b6f254c2d6a544fc3118f07bb13
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-20 22:25:28 +0900

    delete file

--- src/cosin/cleaning_csv.py
+++
@@ -1,8 +0,0 @@
-from utils import cleaning_csv
-from constants import path
-
-PATH_IN = f"{path.DATA}/minami_output"
-PATH_OUT = f"{path.DATA}/processed/csv"
-
-# csvファイルの不要行の削除
-cleaning_csv.delete_columns(PATH_IN, PATH_OUT, 0, 1)
