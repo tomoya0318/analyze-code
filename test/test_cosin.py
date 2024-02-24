efddb24b57d0c57cdce23e8c725b639c4b016e5a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-24 23:50:11 +0900

    add test

--- test/test_cosin.py
+++ test/test_cosin.py
@@ -1,22 +1,27 @@
 import unittest
 from constants import path
 from cosin import lookup_from_model
-from cosin import calc_cosin
+from cosin import calc_cosin, count_values_exceeding_limit
 class TestCosin(unittest.TestCase):
     def setUp(self):
-        self.project_name = "python-sdk"
-        self.input_path = f"{path.PRERESULT}/raw_model/{self.project_name}_train.csv"
+        self.v1 = lookup_from_model(f"{path.PRERESULT}/raw_model/python-sdk_train.csv")
+        self.v2 = lookup_from_model(f"{path.PRERESULT}/raw_model/GPflow_train.csv")
 
     def test_学習データの規約修正率の収集(self):
-        result_dict = lookup_from_model(self.input_path)
-        first_item = list(result_dict.items())[0]
-        self.assertTrue(first_item == ('C0103', 0.95), "The first item is not 'C0103': 0.95")
+        first_item = list(self.v1.items())[0]
+        self.assertTrue(first_item == ('C0103', [19, 19]), "The first item is different from expectations")
     
     def test_コサイン類似度測定(self):
-        v1 = lookup_from_model(self.input_path)
-        v2 = lookup_from_model(f"{path.PRERESULT}/raw_model/GPflow_train.csv")
-        cos_sim, count = calc_cosin(v1, v2)
+        cos_sim = calc_cosin(self.v1, self.v2)
         self.assertTrue(cos_sim, int)
+
+    def test_規約存在数測定(self):
+        non_count = count_values_exceeding_limit(self.v1, self.v2, 0.2)
+        self.assertTrue(non_count, int)
+
+    def test_単体での規約の存在数測定(self):
+        convention_count = count_values_exceeding_limit(self.v1, border=0.2)
+        self.assertTrue(convention_count, int)
 
 if __name__ == '__main__':
     unittest.main()
