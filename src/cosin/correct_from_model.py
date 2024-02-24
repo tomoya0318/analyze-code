e4188623617ce9b3712aca5cd9b3d0c4f2889867
tomoya-n <s266227@wakayama-u.ac.jp>
2024-02-24 18:45:59 +0900

    カウントする規約の閾値の追加

--- src/cosin/correct_from_model.py
+++ src/cosin/correct_from_model.py
@@ -12,7 +12,8 @@ def main():
             name1_dict = lookup_from_model(f"{path.PRERESULT}/raw_model/{name1}_train.csv")
             name2_dict = lookup_from_model(f"{path.PRERESULT}/raw_model/{name2}_train.csv")
 
-            cosin_sim, nan_count = calc_cosin(name1_dict, name2_dict)
+            cosin_sim = calc_cosin(name1_dict, name2_dict)
+            nan_count = count_both_none(name1_dict, name2_dict, border=0.1)
             df = pd.concat(
                 [
                     df,
@@ -37,7 +38,7 @@ def main():
     df.to_csv(f"{path.OUT}/all_cosin_similarity.csv", encoding='utf-8', index=False)
 
 def __insert_key_if_none(v1, v2):
-    """比較対象先に特定のキーが含まれていない場合，値を0として追加し，更新された比較先の辞書の値だけをリストとして返す．
+    """比較対象先に特定のキーが含まれていない場合，値をNoneとして追加し，更新された比較先の辞書の値だけをリストとして返す．
 
     Args:
         v1 (dict): 比較先の辞書
@@ -51,7 +52,8 @@ def __insert_key_if_none(v1, v2):
             v1[key] = None
     
     return [v1[key] for key in sorted(v1)]
-        
+
+
 def lookup_from_model(csvfile):
     """csvファイルから規約の修正率を取得
 
@@ -84,17 +86,50 @@ def calc_cosin(v1, v2):
 
     v1 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v1.items()}
     v2 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v2.items()}
-
-    v1_values = __insert_key_if_none(v1, v2)
-    v2_values = __insert_key_if_none(v2, v1)
     
-    v1_nan_0 = [0 if pd.isna(value) else value for value in v1_values]
-    v2_nan_0 = [0 if pd.isna(value) else value for value in v2_values]
+    v1_nan_0 = [0 if pd.isna(value) else value for value in __insert_key_if_none(v1, v2)]
+    v2_nan_0 = [0 if pd.isna(value) else value for value in __insert_key_if_none(v2, v1)]
 
     cosin_sim = format(np.dot(v1_nan_0, v2_nan_0) / (np.linalg.norm(v1_nan_0) * np.linalg.norm(v2_nan_0)), ".2f")
-    nan_count = sum(1 for value1, value2 in zip(v1_values, v2_values) if not pd.isna(value1) and not pd.isna(value2))
 
-    return cosin_sim, nan_count 
+    return cosin_sim
     
+def count_both_none(v1, v2 = None, border = 0.2):
+    """両方のプロジェクトに共通してボーダー以上の修正率の規約が存在する数をカウントするメソッド
+
+    Args:
+        v1 (dict): 1つ目のプロジェクトの規約IDと修正数，発生数が含まれている辞書
+        v2 (dict, optional):　2つ目のプロジェクトの規約IDと修正数，発生数が含まれている辞書. Defaults to None.
+        border (float, optional): 収集する修正率の閾値. Defaults to 0.2.
+
+    Returns:
+        int: 閾値以上の修正率の規約の数
+    """
+    nan_count = 0
+
+    if v2 != None:
+        v1 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v1.items()}
+        v2 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v2.items()}
+
+        v1_values_list = __insert_key_if_none(v1, v2)
+        v2_values_list = __insert_key_if_none(v2, v1)
+
+        for value1, value2 in zip(v1_values_list, v2_values_list):
+            if pd.isna(value1) or pd.isna(value2):
+                continue
+            if value1 >= border and value2 >= border:
+                nan_count += 1
+
+    else:
+        v1 = {key: values[0] / values[1] if values[1] != 0 else 0 for key, values in v1.items()}
+        v1_values_list = [v1[key] for key in sorted(v1)]
+
+        for value1 in v1_values_list:
+            if value1 >= border:
+                nan_count += 1
+
+    return nan_count
+
+
 if __name__ == "__main__":
         main()
